a4c58aad032d6b6a30128abcf3c4194cee528155
Enrique Ortuno <enrique@ortuno.net>
2023-04-14 23:09:50 -0400

    example fixed with docker images separetly created

--- consumer.js
+++ consumer.js
@@ -1,25 +1,47 @@
-const { Kafka } = require("kafkajs");
+const { Kafka, logLevel } = require("kafkajs");
+
+console.log("Starting consumer");
 
 async function consumeMessage() {
   const kafka = new Kafka({
     clientId: "ServiceB",
-    brokers: ["localhost:9092"],
+    groupId: "ServiceB",
+    brokers: ["kafka:9092"],
+    retry: {
+      retries: 200,
+    },
+    logLevel: logLevel.DEBUG,
   });
 
   const consumer = kafka.consumer({ groupId: "test-group" });
-
+  console.log("Connecting to Kafka from consumer");
   // Conectar el consumidor
   await consumer.connect();
 
   // Suscribirse al topic
   await consumer.subscribe({ topic: "test-topic", fromBeginning: true });
 
+  let receivedMessageCount = 0;
+
   // Procesar mensajes
   await consumer.run({
     eachMessage: async ({ topic, partition, message }) => {
-      console.log(`Received message: ${message.value.toString()}`);
+      receivedMessageCount++;
+      console.log(
+        `Received message ${receivedMessageCount}: ${message.value.toString()}`
+      );
     },
   });
 }
 
 consumeMessage().catch(console.error);
+// (async () => {
+//   await Promise.race([
+//     consumeMessage(),
+//     new Promise((resolve) => process.on("SIGTERM", resolve)),
+//   ]);
+//   process.exit(0);
+// })().catch(console.error);
+setInterval(() => {
+  console.log("La aplicación sigue en ejecución");
+}, 1000 * 60); // Cada minuto
--- producer.js
+++ producer.js
@@ -1,26 +1,52 @@
-const { Kafka } = require("kafkajs");
+const { Kafka, logLevel } = require("kafkajs");
+
+console.log("Starting producer");
 
 async function produceMessage() {
   const kafka = new Kafka({
     clientId: "ServiceA",
-    brokers: ["localhost:9092"],
+    brokers: ["kafka:9092"],
+    retry: {
+      retries: 200,
+    },
+    logLevel: logLevel.DEBUG,
   });
 
   const producer = kafka.producer();
-
+  console.log("Connecting to Kafka from producer");
   // Conectar el productor
   await producer.connect();
 
-  // Enviar un mensaje
-  await producer.send({
-    topic: "test-topic",
-    messages: [{ value: "Hello from ServiceA!" }],
-  });
+  let messageCount = 0;
 
-  console.log("Message sent!");
+  // Enviar mensajes de forma infinita cada 10 segundos
+  setInterval(async () => {
+    messageCount++;
 
-  // Desconectar el productor
-  await producer.disconnect();
+    try {
+      await producer.send({
+        topic: "test-topic",
+        acks: 1,
+        messages: [
+          { value: `Hello from ServiceA! Message count: ${messageCount}` },
+        ],
+      });
+
+      console.log(`Message ${messageCount} sent!`);
+    } catch (error) {
+      console.error(`Failed to send message ${messageCount}: ${error.message}`);
+    }
+  }, 10000);
 }
 
 produceMessage().catch(console.error);
+// (async () => {
+//   await Promise.race([
+//     produceMessage(),
+//     new Promise((resolve) => process.on("SIGTERM", resolve)),
+//   ]);
+//   process.exit(0);
+// })().catch(console.error);
+setInterval(() => {
+  console.log("La aplicación sigue en ejecución");
+}, 1000 * 60); // Cada minuto
